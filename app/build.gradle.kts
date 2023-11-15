c93a3e39b31aab266061796b80d3833a9c3ce8c6
Adnan Jelic <adnan.jelic@gmail.com>
2023-11-15 22:18:21 +0100

    Added the version catalog with common dependencies and Ktlint git hook

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,24 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt)
+    kotlin("kapt")
+    alias(libs.plugins.ktlint)
 }
 
 android {
     namespace = "com.adnanjelic.simpletvstreamingapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.adnanjelic.simpletvstreamingapp"
         minSdk = 27
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
         vectorDrawables {
             useSupportLibrary = true
         }
@@ -29,19 +33,24 @@ android {
             )
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
+
     buildFeatures {
         compose = true
     }
+
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.extension.get()
     }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -51,19 +60,48 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    // AndroidX
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.collection.ktx)
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+
+    // DI
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+
+    // Compose
+    implementation(libs.compose.activity)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.bundles.compose)
+
+    // Testing
+    testImplementation(libs.test.junit)
+    testImplementation(libs.test.mockito.kotlin)
+
+    // Storage
+    implementation(libs.androidx.room.ktx)
+    
+    // Networking
+    implementation(libs.bundles.retrofit)
+    implementation(libs.moshi.kotlin)
+
+    kaptAndroidTest(libs.hilt.android.compiler)
+
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.espresso.intents)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.test.compose.ui.junit4)
+    androidTestImplementation(libs.test.android.hilt)
+    androidTestImplementation(libs.test.mockito)
+    androidTestImplementation(libs.test.mockito.kotlin)
+    androidTestImplementation(libs.test.mockito.android)
+
+    // Debug
+    debugImplementation(libs.debug.compose.ui.tooling)
+    debugImplementation(libs.debug.compose.ui.manifest)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,16 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    alias(libs.plugins.android.application) apply(false)
+    alias(libs.plugins.kotlin.android) apply(false)
+    alias(libs.plugins.hilt) apply(false)
+    alias(libs.plugins.com.android.library) apply(false)
+    alias(libs.plugins.kotlin.jvm) apply(false)
+    alias(libs.plugins.ktlint) apply(false)
 }
+
+val installGitHook = tasks.register<Copy>("installGitHook") {
+    from(File(rootProject.rootDir, "automation/pre-commit"))
+    into(File(rootProject.rootDir, ".git/hooks"))
+    fileMode = 0b111111101
+}
+
+tasks.getByPath(":app:preBuild").dependsOn(installGitHook)
