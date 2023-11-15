2b2062508c7ead2e27543a7a98527adfd226693a
Adnan Jelic <adnan.jelic@gmail.com>
2023-11-15 23:02:34 +0100

    Customized the theme

--- app/src/main/java/com/adnanjelic/simpletvstreamingapp/ui/theme/Theme.kt
+++ app/src/main/java/com/adnanjelic/simpletvstreamingapp/ui/theme/Theme.kt
@@ -1,65 +1,50 @@
 package com.adnanjelic.simpletvstreamingapp.ui.theme
 
-import android.app.Activity
 import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
+import androidx.compose.ui.res.colorResource
+import com.adnanjelic.simpletvstreamingapp.R
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+private val DarkColorScheme
+    @Composable get() = darkColorScheme(
+        primary = colorResource(R.color.primary),
+        onPrimary = colorResource(R.color.onPrimary),
+        primaryContainer = colorResource(R.color.primaryContainer),
+        onPrimaryContainer = colorResource(R.color.onPrimaryContainer),
+        secondary = colorResource(R.color.secondary),
+        onSecondary = colorResource(R.color.onSecondary),
+        secondaryContainer = colorResource(R.color.secondaryContainer),
+        onSecondaryContainer = colorResource(R.color.onSecondaryContainer),
+        tertiary = colorResource(R.color.tertiary),
+        onTertiary = colorResource(R.color.onTertiary),
+        tertiaryContainer = colorResource(R.color.tertiaryContainer),
+        onTertiaryContainer = colorResource(R.color.onTertiaryContainer),
+        background = colorResource(R.color.background),
+        onBackground = colorResource(R.color.onBackground),
+        surface = colorResource(R.color.surface),
+        onSurface = colorResource(R.color.onSurface),
+        surfaceVariant = colorResource(R.color.surfaceVariant),
+        onSurfaceVariant = colorResource(R.color.onSurfaceVariant),
+        error = colorResource(R.color.error),
+        onError = colorResource(R.color.onError),
+        errorContainer = colorResource(R.color.errorContainer),
+        onErrorContainer = colorResource(R.color.onErrorContainer),
+    )
 
 @Composable
 fun SimpleTvStreamingAppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
+    val colorScheme = if (dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        val context = LocalContext.current
+        dynamicDarkColorScheme(context)
+    } else {
+        DarkColorScheme
     }
 
     MaterialTheme(
