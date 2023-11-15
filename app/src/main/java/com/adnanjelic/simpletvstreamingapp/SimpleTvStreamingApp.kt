39fd4a74bdd047acd8b546376906fcc5e0c4574f
Adnan Jelic <adnan.jelic@gmail.com>
2023-11-15 23:37:56 +0100

    Created the Hilt app class and implemented it

---
+++ app/src/main/java/com/adnanjelic/simpletvstreamingapp/SimpleTvStreamingApp.kt
@@ -0,0 +1,7 @@
+package com.adnanjelic.simpletvstreamingapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SimpleTvStreamingApp : Application()
