b1700c41f7668a5db6e3835dd0dbc0ffa26d74ee
Adnan Jelic <adnan.jelic@gmail.com>
2023-12-06 06:25:00 +0100

    Added handling of controls to the player

--- feature-video-player/ui/src/main/java/com/adnanjelic/simpletvstreamingapp/videoplayer/ui/component/VideoPlayer.kt
+++ feature-video-player/ui/src/main/java/com/adnanjelic/simpletvstreamingapp/videoplayer/ui/component/VideoPlayer.kt
@@ -1,27 +1,34 @@
 package com.adnanjelic.simpletvstreamingapp.videoplayer.ui.component
 
 import android.content.Context
+import androidx.annotation.OptIn
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.media3.common.MediaItem
+import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
 
+@OptIn(UnstableApi::class)
 @Composable
 fun VideoPlayer(videoUrl: String) {
     Column(modifier = Modifier.fillMaxSize()) {
         val context = LocalContext.current
         val videoPlayer = getPlayer(context, videoUrl)
+        val playerView = PlayerView(context).apply {
+            player = videoPlayer
+            requestFocus()
+        }
 
         AndroidView(
-            factory = {
-                PlayerView(context).apply { player = videoPlayer }
-            }
+            modifier = Modifier.onKeyEvent { playerView.dispatchKeyEvent(it.nativeKeyEvent) },
+            factory = { playerView }
         )
 
         DisposableEffect(Unit) {
