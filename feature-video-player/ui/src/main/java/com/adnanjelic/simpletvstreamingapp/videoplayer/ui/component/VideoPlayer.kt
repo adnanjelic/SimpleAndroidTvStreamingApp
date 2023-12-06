e299ff8e222e4228c49e42b9133ade5dfcaffe8a
Adnan Jelic <adnan.jelic@gmail.com>
2023-12-06 20:13:33 +0100

    Implemented the back button on video player

--- feature-video-player/ui/src/main/java/com/adnanjelic/simpletvstreamingapp/videoplayer/ui/component/VideoPlayer.kt
+++ feature-video-player/ui/src/main/java/com/adnanjelic/simpletvstreamingapp/videoplayer/ui/component/VideoPlayer.kt
@@ -1,34 +1,66 @@
 package com.adnanjelic.simpletvstreamingapp.videoplayer.ui.component
 
 import android.content.Context
+import android.widget.ImageButton
 import androidx.annotation.OptIn
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.media3.common.MediaItem
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
+import com.adnanjelic.simpletvstreamingapp.videoplayer.ui.R
 
 @OptIn(UnstableApi::class)
 @Composable
-fun VideoPlayer(videoUrl: String) {
+fun VideoPlayer(videoUrl: String, onBackSelected: () -> Unit = {}) {
     Column(modifier = Modifier.fillMaxSize()) {
         val context = LocalContext.current
         val videoPlayer = getPlayer(context, videoUrl)
+
         val playerView = PlayerView(context).apply {
+            findViewById<ImageButton>(R.id.back_button).setOnClickListener { onBackSelected() }
             player = videoPlayer
             requestFocus()
         }
 
+        val lifecycle = remember { mutableStateOf(Lifecycle.Event.ON_CREATE) }
+        val lifecycleOwner = LocalLifecycleOwner.current
+        DisposableEffect(lifecycleOwner) {
+            val observer = LifecycleEventObserver { _, event ->
+                lifecycle.value = event
+            }
+            lifecycleOwner.lifecycle.addObserver(observer)
+
+            onDispose {
+                lifecycleOwner.lifecycle.removeObserver(observer)
+            }
+        }
+
         AndroidView(
             modifier = Modifier.onKeyEvent { playerView.dispatchKeyEvent(it.nativeKeyEvent) },
-            factory = { playerView }
+            factory = { playerView },
+            update = {
+                when (lifecycle.value) {
+                    Lifecycle.Event.ON_PAUSE -> {
+                        playerView.onPause()
+                        videoPlayer.pause()
+                    }
+                    Lifecycle.Event.ON_RESUME -> it.onResume()
+                    else -> Unit
+                }
+            }
         )
 
         DisposableEffect(Unit) {
--- feature-video-player/ui/src/main/java/com/adnanjelic/simpletvstreamingapp/videoplayer/ui/screen/VideoPlayerScreen.kt
+++ feature-video-player/ui/src/main/java/com/adnanjelic/simpletvstreamingapp/videoplayer/ui/screen/VideoPlayerScreen.kt
@@ -1,28 +1,15 @@
 package com.adnanjelic.simpletvstreamingapp.videoplayer.ui.screen
 
 import android.widget.Toast
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.tv.material3.ButtonDefaults
 import androidx.tv.material3.ExperimentalTvMaterial3Api
-import androidx.tv.material3.Icon
-import androidx.tv.material3.IconButton
 import com.adnanjelic.simpletvstreamingapp.shared.navigation.model.NavigationDestination
-import com.adnanjelic.simpletvstreamingapp.theme.Dimension
-import com.adnanjelic.simpletvstreamingapp.theme.Green60
 import com.adnanjelic.simpletvstreamingapp.videoplayer.presentation.VideoPlayerViewModel
 import com.adnanjelic.simpletvstreamingapp.videoplayer.presentation.model.VideoPlayerViewState.Loaded
-import com.adnanjelic.simpletvstreamingapp.videoplayer.ui.R
 import com.adnanjelic.simpletvstreamingapp.videoplayer.ui.component.VideoPlayer
 import com.adnanjelic.simpletvstreamingapp.videoplayer.ui.model.VideoPlayerScreenDependencies
 
@@ -40,20 +27,11 @@ fun VideoPlayerScreen(
     val navigation = viewModel.navigationDestination.collectAsStateWithLifecycle(null)
     val notification = viewModel.notification.collectAsStateWithLifecycle(null)
 
-    Box(modifier = Modifier.fillMaxSize()) {
-        if (videoInfo != null) {
-            VideoPlayer(videoUrl = videoInfo.videoUrl)
-        }
-        IconButton(
-            modifier = Modifier.padding(Dimension.Padding.Default),
-            onClick = { viewModel.onNavigateBackSelected() },
-            colors = ButtonDefaults.colors(focusedContainerColor = Green60)
-        ) {
-            Icon(
-                imageVector = Icons.Default.ArrowBack,
-                contentDescription = stringResource(id = R.string.back_label)
-            )
-        }
+    if (videoInfo != null) {
+        VideoPlayer(
+            videoUrl = videoInfo.videoUrl,
+            onBackSelected = { viewModel.onNavigateBackSelected() }
+        )
     }
 
     navigation.value?.let {
